4e4ff49b27a8326a9d9ff5aa64dfb876700a91eb
Yashodeep Nimbekar <117455443+Yashodeeps@users.noreply.github.com>
2023-01-23 18:52:23 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,19 @@
+// h1 tag using react
+
+const heading = React.createElement(
+      "h1",
+     {
+        id: "title",
+     },
+      "Yashodeep's Site"
+    );
+    
+    
+    console.log(heading);
+    
+    const root = ReactDOM.createRoot(document.getElementById("root"));
+    
+    root.render(headings);
+
+
+
